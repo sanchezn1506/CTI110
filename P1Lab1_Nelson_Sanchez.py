01b553f818e8bd779f5ff66abf2e660da8d0d6c3
sanchezn1506 <sanchezn1506@student.faytechcc.edu>
2026-02-08 17:27:14 -0500

    First Python assignment.
    Taking Input from User and Printing Output.

---
+++ P1Lab1_Nelson_Sanchez.py
@@ -0,0 +1,8 @@
+# Nelson Sanchez
+# P1lab1.py
+# input and print
+
+first_name =input("Enter your first name: ")
+last_name =input("Enter your last name: ")
+
+print("Hello,",first_name,last_name +"!","Welcome to CTI-110")
